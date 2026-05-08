6c1b7e12ad17a80be1a970d9bd65ae52a44a8743
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-08 18:53:36 -0400

    feat: Creacion de clase AutorRequest en autores-microservicio

---
+++ autores-microservice/src/main/java/cl/duoc/dsy1103/autores_microservice/dto/AutorRequest.java
@@ -0,0 +1,34 @@
+package cl.duoc.dsy1103.autores_microservice.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AutorRequest {
+
+    @NotBlank(message = "El nombre completo es obligatorio.")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres.")
+    private String nombreCompleto;
+
+    private String biografia;
+
+    @NotBlank(message = "La nacionalidad es obligatoria.")
+    private String nacionalidad;
+
+    @NotNull(message = "La fecha de nacimiento es obligatoria.")
+    @Past(message = "La fecha de nacimiento debe estar en tiempo pasado.")
+    private LocalDateTime fechaNacimiento;
+}
