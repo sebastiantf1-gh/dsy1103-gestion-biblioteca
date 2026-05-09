e3a8e50bf4b8e7fbf079606064d6493aeaa9dd36
Camilo <veravalenzuelacamilo@gmail.com>
2026-05-09 14:42:50 -0400

    feat: creacion de model multas

---
+++ multas-microservice/src/main/java/cl/duoc/multas_microservice/MultasMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.multas_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultasMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultasMicroserviceApplication.class, args);
+	}
+
+}
