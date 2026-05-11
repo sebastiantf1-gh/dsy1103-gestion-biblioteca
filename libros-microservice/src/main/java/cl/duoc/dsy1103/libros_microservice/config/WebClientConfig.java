eac27fb173c44cc843ea3c6fa0ffd8a2f778a321
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-11 14:52:33 -0400

    feat: avance integracion de WebClient en libros-microservice

---
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package cl.duoc.dsy1103.libros_microservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value("${services.autores.url}")
+    private  String autoresServiceUrl;
+
+    @Bean
+    public WebClient autoresWebClient(){
+        return WebClient.builder()
+                .baseUrl(autoresServiceUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Accept", "application/json")
+                .build();
+    }
+
+
+}
