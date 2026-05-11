837230c1e0f7363f5f49046a05bb94f0076b1867
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-11 12:34:45 -0400

    chore: creacion de archivos .sql para creacion de tabla autor e insercion de registros en autores-microservice

---
+++ autores-microservice/src/main/resources/db/migration/V1__create_table_autores.sql
@@ -0,0 +1,8 @@
+CREATE TABLE autor(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre_completo VARCHAR(100) NOT NULL,
+    biografia TEXT,
+    nacionalidad VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
