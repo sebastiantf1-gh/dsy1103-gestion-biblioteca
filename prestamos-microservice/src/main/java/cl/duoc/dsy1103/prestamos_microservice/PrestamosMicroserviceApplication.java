042f229026d27dc855ed15521078697bef0b1e1d
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-11 18:06:37 -0400

    avance prestamos-microservice

---
+++ prestamos-microservice/src/main/java/cl/duoc/dsy1103/prestamos_microservice/PrestamosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.prestamos_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosMicroserviceApplication.class, args);
+	}
+
+}
