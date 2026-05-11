b4ad2f4dfa8f9282b73802c50c1b3c6d72f6f301
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-10 23:25:12 -0400

    feat: creacion de clase LibroMapper en libros-microservice

---
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/mapper/LibroMapper.java
@@ -0,0 +1,36 @@
+package cl.duoc.dsy1103.libros_microservice.mapper;
+
+import cl.duoc.dsy1103.libros_microservice.dto.LibroRequest;
+import cl.duoc.dsy1103.libros_microservice.dto.LibroResponse;
+import cl.duoc.dsy1103.libros_microservice.model.Libro;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibroMapper {
+    public Libro toEntity(LibroRequest request){
+        return Libro.builder()
+                .isbn(request.getIsbn())
+                .titulo(request.getTitulo())
+                .sinopsis(request.getSinopsis())
+                .numeroPaginas(request.getNumeroPaginas())
+                .disponible(request.getDisponible())
+                .idAutor(request.getIdAutor())
+                .idCategoria(request.getIdCategoria())
+                .idGenero(request.getIdGenero())
+                .build();
+    }
+    public LibroResponse toResponse(Libro libro){
+        return LibroResponse.builder()
+                .id(libro.getId())
+                .isbn(libro.getIsbn())
+                .titulo(libro.getTitulo())
+                .sinopsis(libro.getSinopsis())
+                .numeroPaginas(libro.getNumeroPaginas())
+                .disponible(libro.getDisponible())
+                .fechaRegistro(libro.getFechaRegistro())
+                .idAutor(libro.getIdAutor())
+                .idCategoria(libro.getIdCategoria())
+                .idGenero(libro.getIdGenero())
+                .build();
+    }
+}
