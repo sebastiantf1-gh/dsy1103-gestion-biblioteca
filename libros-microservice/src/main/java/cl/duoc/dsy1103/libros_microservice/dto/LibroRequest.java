b00fac5c8829203384456ea5d3eebdb1ed07d47d
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-10 22:57:40 -0400

    feat: creacion de clase LibroRequest en libros-microservice

---
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/dto/LibroRequest.java
@@ -0,0 +1,44 @@
+package cl.duoc.dsy1103.libros_microservice.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LibroRequest {
+    @NotBlank(message = "El ISBN es obligatorio.")
+    @Size(max = 100, message = "El titulo debe tener como maximo 100 caracteres.")
+    private String isbn;
+
+    @NotBlank(message = "El titulo es obligatorio.")
+    private String titulo;
+
+    private String sinopsis;
+
+    @NotNull(message = "El numero de paginas es obligatorio.")
+    private Short numeroPaginas;
+
+    @NotNull(message = "La disponibilidad es obligatoria.")
+    private Boolean disponible;
+
+    @NotNull(message = "El ID de autor es obligatorio.")
+    private Long idAutor;
+
+    @NotNull(message = "El ID de categoria es obligatorio.")
+    private Long idCategoria;
+
+    @NotNull(message = "El ID de genero es obligatorio.")
+    private Long idGenero;
+
+}
