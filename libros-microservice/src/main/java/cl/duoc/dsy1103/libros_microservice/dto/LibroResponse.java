b4ba7be31e3a20fb1e845b13ff8702df948d939f
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-10 23:03:32 -0400

    feat: creacion de clase LibroResponse en libros-microservice

---
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/dto/LibroResponse.java
@@ -0,0 +1,25 @@
+package cl.duoc.dsy1103.libros_microservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LibroResponse {
+    private Long id;
+    private String isbn;
+    private String titulo;
+    private String sinopsis;
+    private Short numeroPaginas;
+    private Boolean disponible;
+    private LocalDateTime fechaRegistro;
+    private Long idAutor;
+    private Long idCategoria;
+    private Long idGenero;
+}
