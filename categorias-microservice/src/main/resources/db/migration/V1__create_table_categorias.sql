440dc4e39daf1d0ec18b634f2596ab417f7e3a8e
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-11 13:27:29 -0400

    chore: creacion de archivos .sql para creacion de tabla categorias e insercion de registros en categorias-microservice

---
+++ categorias-microservice/src/main/resources/db/migration/V1__create_table_categorias.sql
@@ -0,0 +1,6 @@
+CREATE TABLE categorias(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre      VARCHAR(90) NOT NULL,
+    descripcion VARCHAR(250) NOT NULL,
+    id_libro    BIGINT NOT NULL
+);
--- libros-microservice/src/main/resources/db/migration/V1__create_table_libros.sql
+++ libros-microservice/src/main/resources/db/migration/V1__create_table_libros.sql
@@ -1,12 +1,12 @@
 CREATE TABLE libros(
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    isbn VARCHAR(20) NOT NULL UNIQUE,
-    titulo VARCHAR(100) NOT NULL,
-    sinopsis TEXT,
-    numero_paginas SMALLINT NOT NULL,
-    disponible BOOLEAN NOT NULL,
-    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    id_autor BIGINT NOT NULL,
-    id_categoria BIGINT NOT NULL,
-    id_genero BIGINT NOT NULL
+    id              BIGINT PRIMARY KEY AUTO_INCREMENT,
+    isbn            VARCHAR(20) NOT NULL UNIQUE,
+    titulo          VARCHAR(100) NOT NULL,
+    sinopsis        TEXT,
+    numero_paginas  SMALLINT NOT NULL,
+    disponible      BOOLEAN NOT NULL,
+    fecha_registro  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_autor        BIGINT NOT NULL,
+    id_categoria    BIGINT NOT NULL,
+    id_genero       BIGINT NOT NULL
 );
