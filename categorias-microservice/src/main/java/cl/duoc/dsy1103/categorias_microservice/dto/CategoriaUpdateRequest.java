43c8c53c2c72c8de956f729cefa7cae278831b7d
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-11 01:21:51 -0400

    feat: creacion de clase CategoriaUpdateRequest en categorias-microservice

---
+++ categorias-microservice/src/main/java/cl/duoc/dsy1103/categorias_microservice/dto/CategoriaUpdateRequest.java
@@ -0,0 +1,21 @@
+package cl.duoc.dsy1103.categorias_microservice.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoriaUpdateRequest {
+    @Size(max = 90, message = "El nombre debe tener como maximo 90 caracteres.")
+    private String nombre;
+
+    @Size(max = 250, message = "La descripcion debe tener como maximo 250 caracteres.")
+    private String descripcion;
+
+    private Long idLibro;
+}
