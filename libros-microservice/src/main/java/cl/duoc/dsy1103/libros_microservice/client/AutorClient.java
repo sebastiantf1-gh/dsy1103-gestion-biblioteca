f2906125191acd7dabf73ed121d4303abfa80789
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-12 18:30:37 -0400

    feat: Creacion de clase AutorClient en libros-microservice

--- libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/client/AutorClient.java
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/client/AutorClient.java
@@ -1,9 +1,12 @@
 package cl.duoc.dsy1103.libros_microservice.client;
 
+import cl.duoc.dsy1103.libros_microservice.dto.AutorResponse;
+import cl.duoc.dsy1103.libros_microservice.exception.UnauthorizedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Component
 @Slf4j
@@ -11,6 +14,24 @@ public class AutorClient {
 
     @Autowired
     private WebClient autorWebClient;
+
+    public AutorResponse buscarAutorPorId(Long idAutor){
+        log.info("Obteniendo autor con ID: {}", idAutor);
+        try{
+            return autorWebClient.get()
+                    .uri("/autores/{idAutor}")
+                    .retrieve()
+                    .bodyToMono(AutorResponse.class)
+                    .block();
+
+        }catch (WebClientResponseException ex) {
+            log.error("Error al obtener autor con ID: {}", idAutor);
+            switch (ex.getStatusCode().value()){
+                case 404 -> throw new RuntimeException("No se encontró autor con ID " + idAutor);
+                default -> throw new RuntimeException("Error al intentar obtener autor con ID "+ idAutor, ex);
+            }
+        }
+    }
 
 
 }
