559423bb36ce56a5252397e5fdec8d98f4f7908c
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-13 00:19:02 -0400

    feat: creacion de clase JwtAuthenticationFilter en libros-microservice

---
+++ libros-microservice/src/main/java/cl/duoc/dsy1103/libros_microservice/security/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package cl.duoc.dsy1103.libros_microservice.security;
+
+import cl.duoc.dsy1103.libros_microservice.service.JwtService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filter.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        if(jwtService.isTokenValid(token)){
+            Claims claims = jwtService.extractClaims(token);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    claims.getSubject(), null, List.of());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("Token valido para usuario: {}", claims.getSubject());
+        }else {
+            log.warn("Token invalido en solicitud a: {}", request.getRequestURI());
+        }
+        filter.doFilter(request, response);
+    }
+}
