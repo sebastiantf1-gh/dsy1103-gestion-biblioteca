8487f6ff5ce19debaaeb078a42e0a2f3d92bb85f
Camilo <veravalenzuelacamilo@gmail.com>
2026-05-14 10:37:17 -0400

    feat: implementacion de migraciones SQL

---
+++ autenticacion-microservice/src/main/resources/db/migration/V2_initial_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO personal_biblioteca (nombre_completo, password, email, telefono, fecha_registro)
+VALUES ('camilo_admin', 'password123', 'camilo.biblioteca@duoc.cl', '+56912345678', NOW());
+
+INSERT INTO personal_biblioteca (nombre_completo, password, email, telefono, fecha_registro)
+VALUES ('marcela_bibliotecaria', 'admin2024', 'marcela.perez@duoc.cl', '+56987654321', NOW());
+
+INSERT INTO personal_biblioteca (nombre_completo, password, email, telefono, fecha_registro)
+VALUES ('juan_soporte', 'duoc2024', 'juan.soporte@duoc.cl', '+56955554433', NOW());
