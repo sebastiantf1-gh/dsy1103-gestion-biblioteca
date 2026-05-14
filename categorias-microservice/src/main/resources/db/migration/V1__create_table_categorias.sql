a2d7de59b2f436e357964c7baac2e01bf2daa47c
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-14 01:05:34 -0400

    fix: se actualizan registros de archivos initial_data.sql por consistencia con nueva relacion 1:N

--- categorias-microservice/src/main/resources/db/migration/V1__create_table_categorias.sql
+++ categorias-microservice/src/main/resources/db/migration/V1__create_table_categorias.sql
@@ -1,6 +1,5 @@
 CREATE TABLE categorias(
     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     nombre      VARCHAR(90) NOT NULL,
-    descripcion VARCHAR(250) NOT NULL,
-    id_libro    BIGINT NOT NULL
+    descripcion VARCHAR(250) NOT NULL
 );
