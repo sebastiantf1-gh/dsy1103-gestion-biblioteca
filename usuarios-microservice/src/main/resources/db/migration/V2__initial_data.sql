d0bf08e8999495afd1c5737ffd050c788fe1104d
Sebastian Toro <sebastian.toroforte@gmail.com>
2026-05-15 18:45:57 -0400

    fix: se corrige sintaxis de sript sql de insercion de registros en tabla usuarios de usuarios-microservice

--- usuarios-microservice/src/main/resources/db/migration/V2__initial_data.sql
+++ usuarios-microservice/src/main/resources/db/migration/V2__initial_data.sql
@@ -4,7 +4,7 @@ INSERT INTO usuarios (nombre_completo, email, telefono) VALUES
 ('Homero J', 'homero.j@duoc.com', '+56958745874'),
 ('Lisa Marie','lisam.thom@duoc.com','+56912378945'),
 ('Hugo', 'hugo.j@duoc.com', '+56958445874'),
-('krusty','theclown@duoc.com','+56912546945')
+('krusty','theclown@duoc.com','+56912546945'),
 ('Ned Flanders', 'n.flanders@duoc.com', '+56987654321'),
 ('Barney Gumble', 'b.gumble@duoc.com', '+56976543210'),
 ('John Marston', 'j.marston@duoc.com', '+56965432109'),
