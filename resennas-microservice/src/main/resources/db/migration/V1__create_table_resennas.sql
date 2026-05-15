20dd7f0c26e63e2e484652537f594a697bc1301d
benjav892 <benjam.vasquezc@duocuc.cl>
2026-05-15 12:36:00 -0400

    refactor: elimina la constrain del script sql v1

--- resennas-microservice/src/main/resources/db/migration/V1__create_table_resennas.sql
+++ resennas-microservice/src/main/resources/db/migration/V1__create_table_resennas.sql
@@ -1,9 +1,9 @@
 CREATE TABLE resennas(
-    id              BIGINT NOT NULL AUTO_INCREMENT,
+    id              BIGINT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
     descripcion     VARCHAR(255),
     calificacion    INT NOT NULL,
     fecha_registro  DATETIME NOT NULL,
     id_usuario      BIGINT NOT NULL,
     id_libro        BIGINT NOT NULL,
-    CONSTRAINT pk_resennas PRIMARY KEY (id)
+
 );
