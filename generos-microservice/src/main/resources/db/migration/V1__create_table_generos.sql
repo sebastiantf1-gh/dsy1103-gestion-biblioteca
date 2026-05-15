52307e34af2a05c5d1b015ed6fe20d990ebce501
benjav892 <benjam.vasquezc@duocuc.cl>
2026-05-15 02:08:01 -0400

    refactor: elimina la constrain del script sql v1

--- generos-microservice/src/main/resources/db/migration/V1__create_table_generos.sql
+++ generos-microservice/src/main/resources/db/migration/V1__create_table_generos.sql
@@ -1,6 +1,5 @@
 CREATE TABLE generos(
     id BIGINT NOT NULL AUTO_INCREMENT,
     nombre_genero VARCHAR(100) NOT NULL UNIQUE,
-    descripcion VARCHAR(255) NOT NULL,
-    CONSTRAINT  pk_usuarios PRIMARY KEY (id)
+    descripcion VARCHAR(255) NOT NULL
 );
