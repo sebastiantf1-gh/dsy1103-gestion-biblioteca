76663849ab63ac8ffcec43ee6ca7d647affe4d3e
Camilo <veravalenzuelacamilo@gmail.com>
2026-05-16 22:22:32 -0400

    feat: Integracion de reservas microservice

---
+++ reservas-microservice/src/main/java/cl/duoc/reservas_microservice/ReservasMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.reservas_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasMicroserviceApplication.class, args);
+	}
+
+}
---
+++ reservas-microservice/src/test/java/cl/duoc/reservas_microservice/ReservasMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.reservas_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
