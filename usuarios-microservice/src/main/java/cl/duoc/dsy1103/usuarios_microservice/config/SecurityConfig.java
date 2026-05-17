a5f32b463ff17f1f68da84b451d31150a3b7d11d
benjav892 <benjam.vasquezc@duocuc.cl>
2026-05-17 18:12:48 -0400

    test: se modifica SecurityConfig de usuarios,resennas y generos para activar seguridad

--- usuarios-microservice/src/main/java/cl/duoc/dsy1103/usuarios_microservice/config/SecurityConfig.java
+++ usuarios-microservice/src/main/java/cl/duoc/dsy1103/usuarios_microservice/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package cl.duoc.dsy1103.usuarios_microservice.config;
 
+import cl.duoc.dsy1103.usuarios_microservice.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration //con esta anotacion le decimos a Sring que esta clase tiene beans
 @EnableWebSecurity //activamos el soporte de seguridad web de Spring
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -19,11 +26,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 // Configuramos la politica de sesiones como STATELESS (sin estado) porque los microservicios no guardan sesiones en el servidor
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-                /*se comenta lo siguiente para desactivar seguridad
-                auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        // Permite que cualquiera pueda registrarse
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-                 */
+
         return http.build();
     }
 
